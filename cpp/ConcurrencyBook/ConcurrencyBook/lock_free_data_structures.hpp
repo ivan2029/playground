131f0f2ec09ad7e64973c18fb7fb67fc769febe4
Ivan Barisic <ivan.barisic@endava.com>
2018-03-14 14:48:21 +0100

    prepare for lock free structures

--- cpp/ConcurrencyBook/ConcurrencyBook/lock_free_data_structures.hpp
+++ cpp/ConcurrencyBook/ConcurrencyBook/lock_free_data_structures.hpp
@@ -7,6 +7,26 @@ namespace threadsafe {
     //
     //
     //
+    template<class T>
+    class StackLockFree {
+    public: // ctor, dtor, assign
+
+        StackLockFree() = default;
+
+        StackLockFree(StackLockFree const& ) = delete;
+
+        StackLockFree(StackLockFree&& ) = delete;
+
+        ~StackLockFree() = default;
+
+        StackLockFree& operator= (StackLockFree const& ) = delete;
+
+        StackLockFree& operator= (StackLockFree&& ) = delete;
+
+    public:
+
+    private:
+    };
 
 
 }
